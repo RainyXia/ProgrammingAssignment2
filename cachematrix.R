477d2235ad5a029d43023d5e5f277a47fb814123
RainyXia <xiaoxue_rainy@hotmail.com>
2014-12-19 13:53:08 +0800

    Completed the makeCachaMatrixed function
    Completed the makeCachaMatrixed function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        n <- NULL
+        set <- function(y) {
+                x <<- y
+                n <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) n <<- solve
+        getsolve <- function() n
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)  
 }
 
 
