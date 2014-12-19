05b5d8e7731ee490d382438f837230a8bd845ab4
RainyXia <xiaoxue_rainy@hotmail.com>
2014-12-19 13:59:08 +0800

    添加comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 编写可以缓存逆矩阵的函数对
 
-## Write a short comment describing this function
+## 此函数用于创建可缓存逆矩阵的特殊“矩阵”对象
 
 makeCacheMatrix <- function(x = matrix()) {
         n <- NULL
@@ -18,7 +17,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## 此函数用于计算上述makeCacheMatrix返回的特殊“矩阵”的逆矩阵。
+##如果已经计算逆矩阵（且尚未更改矩阵），那么cachesolve将检索缓存中的逆矩阵
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
